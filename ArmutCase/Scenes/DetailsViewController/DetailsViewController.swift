5c212da2bc701417063ba60fdcaa0c28ba79400e
Can Kurtur <>
2021-12-07 14:15:22 +0300

    [A-31] DetailsVC fixed.

--- ArmutCase/Scenes/DetailsViewController/DetailsViewController.swift
+++ ArmutCase/Scenes/DetailsViewController/DetailsViewController.swift
@@ -11,16 +11,21 @@ class DetailsViewController: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet private weak var serviceAndDetailsView: ServiceAndDetailsView!
     @IBOutlet private weak var workInformationView: WorkInformationView!
+    @IBOutlet private weak var emptyView: UIView!
 
     // MARK: - Properties
     private var viewModel = DetailsViewModel()
     var serviceID: Int?
+
     // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         setupBindings()
     }
 
+//    deinit {
+//        emptyView.isHidden = true
+//    }
     // MARK: - Business Logic
     private func setupBindings() {
         guard let serviceID = serviceID else { return }
@@ -33,7 +38,13 @@ class DetailsViewController: UIViewController {
     private func configureViews(model: DetailsUIModel) {
         let serviceDetailsViewModel = ServiceAndDetailsViewModel(detailsUIModel: model)
         serviceAndDetailsView.configure(viewModel: serviceDetailsViewModel)
-
         workInformationView.configure(viewModel: viewModel.workAndInformationViewModel)
+    }
+}
+
+// MARK: - ErrorDelegate
+extension DetailsViewController: ServiceErrorDelegate {
+    func throwEror() {
+        emptyView.isHidden = false
     }
 }
